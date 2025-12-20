8035710c1c72dbbde31b971e516d48649e2085f8
MS-Kwak <kkminseo@gmail.com>
2025-12-20 19:56:48 +0900

    46. (6.6) 인증 에러 처리하기 1

--- src/hooks/mutations/use-sign-in-with-password.ts
+++ src/hooks/mutations/use-sign-in-with-password.ts
@@ -1,9 +1,14 @@
 import { signInWithPassword } from "@/api/auth";
+import type { UseMutationCallback } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
-export function useSignInWithPassword() {
+export function useSignInWithPassword(callbacks?: UseMutationCallback) {
   return useMutation({
     mutationFn: signInWithPassword,
+    onError: (error: Error) => {
+      console.error(error);
+
+      if (callbacks?.onError) callbacks.onError(error);
+    },
   });
 }
-
